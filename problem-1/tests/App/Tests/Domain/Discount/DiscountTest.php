1bb6d666a3a9bb8a5dfc9ec7408f531332e84154
Tommy Kao <30006459+tmy2017@users.noreply.github.com>
2023-11-14 13:18:47 +0100

    feat(discount) first green test: discount when only one order

--- problem-1/tests/App/Tests/Domain/Discount/DiscountTest.php
+++ problem-1/tests/App/Tests/Domain/Discount/DiscountTest.php
@@ -6,34 +6,42 @@ use PHPUnit\Framework\TestCase;
 
 class DiscountTest extends TestCase
 {
+    /**
+     * @param string $filename
+     * @return void
+     */
+    public function getOrdersFromJson(string $filename): Order
+    {
+        if (file_exists($filename)) {
+            $contents = file_get_contents($filename);
+            return new Order(json_decode($contents, true));
+        }
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
-        $filename = MONO_REPO_ROOT . '/example-orders/order1.json';
-        if (file_exists($filename)) {
-            $contents = file_get_contents($filename);
-            $this->order = new Order(json_decode($contents, true));
-        }
+        $this->sampleOrder1 = $this->getOrdersFromJson(MONO_REPO_ROOT . '/example-orders/order1.json');
+        $this->sampleOrder1a = $this->getOrdersFromJson(MONO_REPO_ROOT . '/example-orders/order1a.json');
     }
 
     public function test_can_read_data_from_incoming_data_source()
     {
-        //Act
-
-        //Assert
-        $this->assertSame(4.99, $this->order->getItems()[0]->getUnitPrice());
-
+        $this->assertSame(4.99, $this->sampleOrder1->getItems()[0]->getUnitPrice());
     }
 
     public function test_give_10_percent_discount_for_only_one_order_which_in_total_over_1000_euro(): void
     {
         //Arrange
-        $totalPrice = 42;
+        $order = $this->sampleOrder1a;
+        $totalPrice = 1049.90;
 
         //Act
+        $calc = new DiscountCalculator($order, new Over1000Discount10PercentDiscount());
+        $discountResult = $calc->calculateDiscountAndReason();
 
         //Assert
-        $this->assertSame($this->discountCalculator->calculateDiscount(), $totalPrice * 0.9);
+        $this->assertEquals($totalPrice * 0.9, $discountResult->getTotalAfterDiscount());
     }
 
     public function test_for_every_product_of_category_switches_buy_five_get_sixth_free(): void
@@ -48,13 +56,68 @@ class DiscountTest extends TestCase
 
 }
 
+class DiscountCalculator
+{
+    private Order $order;
+    private DiscountStrategy $discountStrategy;
+
+    public function __construct(Order $order, DiscountStrategy $discountStrategy)
+    {
+        $this->order = $order;
+        $this->discountStrategy = $discountStrategy;
+    }
+
+    public function calculateDiscountAndReason(): Discount
+    {
+        return $this->discountStrategy->calculateDiscountAndReason($this->order);
+    }
+}
+
+interface DiscountStrategy
+{
+    public function calculateDiscountAndReason(Order $order): Discount;
+}
+
+class Over1000Discount10PercentDiscount implements DiscountStrategy
+{
+
+    public function calculateDiscountAndReason(Order $order): Discount
+    {
+        $totalAfterDiscount = 0.0;
+        $discountReason = "";
+
+        if ($order->getTotal() > 1000) {
+            $totalAfterDiscount = $order->getTotal() * 0.9;
+            $discountReason = DiscountReasons::A_CUSTOMER_WHO_HAS_ALREADY_BOUGHT_FOR_OVER_1000_GETS_A_DISCOUNT_OF_10_ON_THE_WHOLE_ORDER;
+        }
+        return new Discount($totalAfterDiscount, $discountReason);
+    }
+}
+
+class Discount
+{
+    private float $totalAfterDiscount;
+    private string $discountReason;
+
+    public function __construct(float $totalAfterDiscount, string $discountReason)
+    {
+        $this->totalAfterDiscount = $totalAfterDiscount;
+        $this->discountReason = $discountReason;
+    }
+
+    public function getTotalAfterDiscount(): float
+    {
+        return $this->totalAfterDiscount;
+    }
+
+}
 
 class Order
 {
     private int $id;
     private int $customerId;
     private array $items;
-    private int $total;
+    private float $total;
 
     /**
      * @param $data
@@ -72,6 +135,11 @@ class Order
     public function getItems(): array
     {
         return $this->items;
+    }
+
+    public function getTotal(): float
+    {
+        return $this->total;
     }
 
 }
@@ -100,4 +168,11 @@ class OrderItem
         return $this->unitPrice;
     }
 
+}
+
+
+class DiscountReasons
+{
+
+    public const A_CUSTOMER_WHO_HAS_ALREADY_BOUGHT_FOR_OVER_1000_GETS_A_DISCOUNT_OF_10_ON_THE_WHOLE_ORDER = "A customer who has already bought for over € 1000, gets a discount of 10% on the whole order. ";
 }
