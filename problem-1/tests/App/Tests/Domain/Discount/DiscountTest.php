150c3dd17fde44805533050df0ec2a481794d38f
Tommy Kao <30006459+tmy2017@users.noreply.github.com>
2023-11-14 09:10:14 +0100

    chores(phpunit) add phpunit and tests to project

---
+++ problem-1/tests/App/Tests/Domain/Discount/DiscountTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Domain\Discount;
+
+use PHPUnit\Framework\TestCase;
+
+//define('PROJECT_ROOT', )
+
+class DiscountTest extends TestCase
+{
+    public function testGive10PercentDiscountForOrdersOver1000Euro()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        $this->assertSame(42, 42);
+    }
+
+    public function testForEveryProductOfCategorySwitchesBuyFiveGetSixthFree()
+    {
+
+    }
+
+    public function testBuyTwoOrMoreToolsGet20PercentDiscountOnCheapestProduct()
+    {
+
+    }
+
+}
