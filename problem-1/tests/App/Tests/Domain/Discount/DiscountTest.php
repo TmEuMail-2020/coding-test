d4985f4976abae52d3bdacf4d4f7740c70893e8d
Tommy Kao <30006459+tmy2017@users.noreply.github.com>
2023-11-14 15:26:14 +0100

    chores(discount) add test for multiple items can also calculate discount correctly
    Note:  due to "total" is already provided in response, hence this test case is only for consistency. Meaning test case for singular, then test case for plural.

--- problem-1/tests/App/Tests/Domain/Discount/DiscountTest.php
+++ problem-1/tests/App/Tests/Domain/Discount/DiscountTest.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests\Domain\Discount;
 
+use App\Domain\Discount\DiscountCalculator;
 use App\Domain\Discount\Order;
+use App\Domain\Discount\Over1000Discount10PercentIDiscount;
 use PHPUnit\Framework\TestCase;
 
 class DiscountTest extends TestCase
@@ -24,6 +26,7 @@ class DiscountTest extends TestCase
         parent::setUp();
         $this->sampleOrder1 = $this->getOrdersFromJson(MONO_REPO_ROOT . '/example-orders/order1.json');
         $this->sampleOrder1a = $this->getOrdersFromJson(MONO_REPO_ROOT . '/example-orders/order1a.json');
+        $this->sampleOrder3a = $this->getOrdersFromJson(MONO_REPO_ROOT . '/example-orders/order3a.json');
     }
 
     public function test_can_read_data_from_incoming_data_source()
@@ -31,14 +34,27 @@ class DiscountTest extends TestCase
         $this->assertSame(4.99, $this->sampleOrder1->getItems()[0]->getUnitPrice());
     }
 
-    public function test_give_10_percent_discount_for_only_one_order_which_in_total_over_1000_euro(): void
+    public function test_give_10_percent_discount_for_only_one_item_which_in_total_over_1000_euro(): void
     {
         //Arrange
         $order = $this->sampleOrder1a;
         $totalPrice = 1049.90;
 
         //Act
-        $calc = new \App\Domain\Discount\DiscountCalculator($order, new \App\Domain\Discount\Over1000Discount10PercentIDiscount());
+        $calc = new DiscountCalculator($order, new Over1000Discount10PercentIDiscount());
+        $discountResult = $calc->calculateDiscountAndReason();
+
+        //Assert
+        $this->assertEquals($totalPrice * 0.9, $discountResult->getTotalAfterDiscount());
+    }
+    public function test_give_10_percent_discount_for_multiple_items_which_in_total_over_1000_euro(): void
+    {
+        //Arrange
+        $order = $this->sampleOrder3a;
+        $totalPrice = 1069.00;
+
+        //Act
+        $calc = new DiscountCalculator($order, new Over1000Discount10PercentIDiscount());
         $discountResult = $calc->calculateDiscountAndReason();
 
         //Assert
