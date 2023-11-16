f4eddeefb6de9966624860d04e80a53e92688e67
Tommy Kao <30006459+tmy2017@users.noreply.github.com>
2023-11-16 22:44:57 +0100

    feat(webapp) Able to accept incoming json request. Add jetbrains http-client tool in tools/ folder for quick testing.

--- problem-1/public/index.php
+++ problem-1/public/index.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Domain\Discount\DiscountCalculator;
-use App\Domain\Discount\EverySixthCategorySwitchDiscount;
-use App\Domain\Discount\Over1000TotalThen10PercentDiscount;
-use App\Domain\Discount\TwoOrMoreCategoryToolsGet20PercentDiscount as TwoOrMoreCategoryToolsGet20PercentDiscountAlias;
+use App\Domain\Discount\Model\Order;
+use App\Domain\Discount\Model\Strategy\EverySixthCategorySwitchDiscount;
+use App\Domain\Discount\Model\Strategy\Over1000TotalThen10PercentDiscount;
+use App\Domain\Discount\Model\Strategy\TwoOrMoreCategoryToolsGet20PercentDiscount as TwoOrMoreCategoryToolsGet20PercentDiscountAlias;
+use App\Domain\Discount\Service\DiscountCalculator;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
@@ -11,53 +12,42 @@ use Slim\Psr7\Response;
 require __DIR__ . '/../vendor/autoload.php';
 define('PROJECT_ROOT', dirname(__DIR__, 2));
 
-
-$sampleOrder1 = DiscountCalculator::getOrdersFromJson(PROJECT_ROOT . '/example-orders/order1.json');
-$sampleOrder1a = DiscountCalculator::getOrdersFromJson(PROJECT_ROOT . '/example-orders/order1a.json');
-$sampleOrder3a = DiscountCalculator::getOrdersFromJson(PROJECT_ROOT . '/example-orders/order3a.json');
-$sampleOrder3 = DiscountCalculator::getOrdersFromJson(PROJECT_ROOT . '/example-orders/order3.json');
 $productDictionary = DiscountCalculator::getProductDictionaryFromJson(PROJECT_ROOT . '/data/products.json');
 
 $app = AppFactory::create();
-$app->get(
-    '/calculateOver1000TotalThen10PercentDiscount',
-    function (Request $request, Response $response, $args) use ($sampleOrder1a) {
-        $order = $sampleOrder1a;
+//  Out of the box, PSR-7 implementations do not support these formats, hence middleware is needed
+//      https://stackoverflow.com/a/57524022/22349977
+//      https://www.slimframework.com/docs/v4/middleware/body-parsing.html
+$app->addBodyParsingMiddleware();
+$app->post(
+    '/discount/strategy/{id}',
+    function (Request $request, Response $response, array $args) use ($productDictionary) {
+        $id = $args['id'];
+        $order = new Order($request->getParsedBody(), true);
 
-        //Act
-        $calc = new DiscountCalculator($order, new Over1000TotalThen10PercentDiscount());
-        $discountResult = $calc->calculateDiscountAndReason();
-
-        $response->getBody()->write(json_encode($discountResult));
-        return $response->withHeader('Content-Type', 'application/json');
+        try {
+            $calc = match ($id) {
+                '1' => new DiscountCalculator(
+                    $order, new Over1000TotalThen10PercentDiscount()
+                ),
+                '2' => new DiscountCalculator(
+                    $order,
+                    new EverySixthCategorySwitchDiscount($productDictionary)
+                ),
+                '3' => new DiscountCalculator(
+                    $order,
+                    new TwoOrMoreCategoryToolsGet20PercentDiscountAlias($productDictionary)
+                ),
+                default => throw new Exception("Unknown strategy ID: $id"),
+            };
+            $discountResult = $calc->calculateDiscountAndReason();
+            $response->getBody()->write(json_encode($discountResult));
+            return $response->withHeader('Content-Type', 'application/json');
+        } catch (Exception $e) {
+            $response->getBody()->write(json_encode($e->getMessage()));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+        }
     }
 );
-$app->get(
-    '/',
-    function (Request $request, Response $response, $args) {
-        $response->getBody()->write(json_encode("Hello World"));
-        return $response->withHeader('Content-Type', 'application/json');
-    }
-);
-$app->get('/calculateEverySixthCategorySwitchDiscount', function (Request $request, Response $response, $args) use ($sampleOrder1a, $productDictionary) {
-    $order = $sampleOrder1a;
-
-    //Act
-    $calc = new DiscountCalculator($order, new EverySixthCategorySwitchDiscount($productDictionary));
-    $discountResult = $calc->calculateDiscountAndReason();
-
-    $response->getBody()->write(json_encode($discountResult));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-$app->get('/calculateTwoOrMoreCategoryToolsGet20PercentDiscountAlias', function (Request $request, Response $response, $args) use ($sampleOrder1a, $productDictionary) {
-    $order = $sampleOrder1a;
-
-    //Act
-    $calc = new DiscountCalculator($order, new TwoOrMoreCategoryToolsGet20PercentDiscountAlias($productDictionary));
-    $discountResult = $calc->calculateDiscountAndReason();
-
-    $response->getBody()->write(json_encode($discountResult));
-    return $response->withHeader('Content-Type', 'application/json');
-});
 
 $app->run();
